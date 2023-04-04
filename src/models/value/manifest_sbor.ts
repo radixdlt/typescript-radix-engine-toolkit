56b7d5f7ea1419a8171f069d5b96bd4c25a34b27
Omar <OmarAbdulla7@hotmail.com>
2023-04-05 01:56:02 +0300

    Manifest SBOR value model

--- src/models/value/manifest_sbor.ts
+++ src/models/value/manifest_sbor.ts
@@ -15,4 +15,663 @@
 // specific language governing permissions and limitations
 // under the License.
 
-export {};
+// TODO: Add to string methods
+// TODO: Convert U64 and I64 to use BigInt
+
+import {
+  bigIntToString,
+  numberToString,
+  stringToBigInt,
+  stringToNumber,
+  stringToUint8Array,
+  uint8ArrayToString,
+} from "../../utils";
+
+export type Value =
+  | Bool
+  | U8
+  | U16
+  | U32
+  | U64
+  | U128
+  | I8
+  | I16
+  | I32
+  | I64
+  | I128
+  | String
+  | Enum
+  | Array
+  | Map
+  | Tuple
+  | Address
+  | Bucket
+  | Proof
+  | Decimal
+  | PreciseDecimal
+  | NonFungibleLocalId
+  | Expression
+  | Blob;
+
+export enum Kind {
+  Bool = "Bool",
+  U8 = "U8",
+  U16 = "U16",
+  U32 = "U32",
+  U64 = "U64",
+  U128 = "U128",
+  I8 = "I8",
+  I16 = "I16",
+  I32 = "I32",
+  I64 = "I64",
+  I128 = "I128",
+  String = "String",
+  Enum = "Enum",
+  Array = "Array",
+  Map = "Map",
+  Tuple = "Tuple",
+  Address = "Address",
+  Bucket = "Bucket",
+  Proof = "Proof",
+  Decimal = "Decimal",
+  PreciseDecimal = "PreciseDecimal",
+  NonFungibleLocalId = "NonFungibleLocalId",
+  Expression = "Expression",
+  Blob = "Blob",
+}
+
+export class Bool {
+  private _type: Kind = Kind.Bool;
+  private _value: boolean;
+
+  public get value(): boolean {
+    return this._value;
+  }
+  public set value(value: boolean) {
+    this._value = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: boolean) {
+    this._value = value;
+  }
+}
+
+export class U8 {
+  private _type: Kind = Kind.U8;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class U16 {
+  private _type: Kind = Kind.U16;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class U32 {
+  private _type: Kind = Kind.U32;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class U64 {
+  private _type: Kind = Kind.U64;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class U128 {
+  private _type: Kind = Kind.U128;
+  private _value: string;
+
+  get value(): BigInt {
+    return stringToBigInt(this._value);
+  }
+
+  set value(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+}
+
+export class I8 {
+  private _type: Kind = Kind.I8;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class I16 {
+  private _type: Kind = Kind.I16;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class I32 {
+  private _type: Kind = Kind.I32;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class I64 {
+  private _type: Kind = Kind.I64;
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class I128 {
+  private _type: Kind = Kind.I128;
+  private _value: string;
+
+  get value(): BigInt {
+    return stringToBigInt(this._value);
+  }
+
+  set value(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+}
+
+export class String {
+  private _type: Kind = Kind.String;
+  private _value: string;
+
+  public get value(): string {
+    return this._value;
+  }
+  public set value(value: string) {
+    this._value = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: string) {
+    this._value = value;
+  }
+}
+
+export class Enum {
+  private _type: Kind = Kind.Enum;
+  private _variant: string;
+  private _fields: globalThis.Array<Value> = [];
+
+  public get variant(): number {
+    return stringToNumber(this._variant);
+  }
+  public set variant(value: number) {
+    this._variant = numberToString(value);
+  }
+
+  public get fields(): globalThis.Array<Value> {
+    return this._fields;
+  }
+  public set fields(value: globalThis.Array<Value>) {
+    this._fields = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(variant: number, fields: globalThis.Array<Value> = []) {
+    this._variant = numberToString(variant);
+    this.fields = fields;
+  }
+}
+
+export class Array {
+  private _type: Kind = Kind.Array;
+  private _elementKind: Kind;
+  private _elements: globalThis.Array<Value>;
+
+  public get elementKind(): Kind {
+    return this._elementKind;
+  }
+  public set elementKind(value: Kind) {
+    this._elementKind = value;
+  }
+
+  public get elements(): globalThis.Array<Value> {
+    return this._elements;
+  }
+  public set elements(value: globalThis.Array<Value>) {
+    this._elements = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(elementKind: Kind, elements: globalThis.Array<Value>) {
+    this._elementKind = elementKind;
+    this._elements = elements;
+  }
+}
+
+export class Map {
+  private _type: Kind = Kind.Map;
+  private _keyValueKind: Kind;
+  private _valueValueKind: Kind;
+  private _entries: globalThis.Array<[Value, Value]> = [];
+
+  public get keyValueKind(): Kind {
+    return this._keyValueKind;
+  }
+  public set keyValueKind(value: Kind) {
+    this._keyValueKind = value;
+  }
+
+  public get valueValueKind(): Kind {
+    return this._valueValueKind;
+  }
+  public set valueValueKind(value: Kind) {
+    this._valueValueKind = value;
+  }
+
+  public get entries(): globalThis.Array<[Value, Value]> {
+    return this._entries;
+  }
+  public set entries(value: globalThis.Array<[Value, Value]>) {
+    this._entries = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(
+    keyValueKind: Kind,
+    valueValueKind: Kind,
+    elements: globalThis.Array<[Value, Value]> = []
+  ) {
+    this._keyValueKind = keyValueKind;
+    this._valueValueKind = valueValueKind;
+    this._entries = elements;
+  }
+}
+
+export class Tuple {
+  private _type: Kind = Kind.Tuple;
+  private _elements: globalThis.Array<Value> = [];
+
+  public get elements(): globalThis.Array<Value> {
+    return this._elements;
+  }
+  public set elements(value: globalThis.Array<Value>) {
+    this._elements = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(elements: globalThis.Array<Value> = []) {
+    this.elements = elements;
+  }
+}
+
+export class Decimal {
+  private _type: Kind = Kind.Decimal;
+  private _value: string;
+
+  get value(): BigInt {
+    return stringToBigInt(this._value);
+  }
+
+  set value(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+}
+
+export class PreciseDecimal {
+  private _type: Kind = Kind.PreciseDecimal;
+  private _value: string;
+
+  get value(): BigInt {
+    return stringToBigInt(this._value);
+  }
+
+  set value(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+}
+
+export class Address {
+  private _type: Kind = Kind.Address;
+  private _address: string;
+
+  public get address(): string {
+    return this._address;
+  }
+  public set address(value: string) {
+    this._address = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(address: string) {
+    this._address = address;
+  }
+}
+
+export class Bucket {
+  private _type: Kind = Kind.Bucket;
+  private _identifier: String | U32;
+
+  public get identifier(): String | U32 {
+    return this._identifier;
+  }
+  public set identifier(value: String | U32) {
+    this._identifier = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(identifier: String | U32) {
+    this._identifier = identifier;
+  }
+}
+
+export class Proof {
+  private _type: Kind = Kind.Proof;
+  private _identifier: String | U32;
+
+  public get identifier(): String | U32 {
+    return this._identifier;
+  }
+  public set identifier(value: String | U32) {
+    this._identifier = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(identifier: String | U32) {
+    this._identifier = identifier;
+  }
+}
+
+export class Expression {
+  private _type: Kind = Kind.Expression;
+  private _value: string;
+
+  public get value(): string {
+    return this._value;
+  }
+  public set value(value: string) {
+    this._value = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(expression: string) {
+    this._value = expression;
+  }
+
+  static entireWorktop(): Expression {
+    return new Expression("ENTIRE_WORKTOP");
+  }
+
+  static entireAuthZone(): Expression {
+    return new Expression("ENTIRE_AUTH_ZONE");
+  }
+}
+
+export class NonFungibleLocalId {
+  private _type: Kind = Kind.NonFungibleLocalId;
+  private _value: UUID | Integer | String | Bytes;
+
+  public get value(): UUID | Integer | String | Bytes {
+    return this._value;
+  }
+  public set value(value: UUID | Integer | String | Bytes) {
+    this._value = value;
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(value: UUID | Integer | String | Bytes) {
+    this._value = value;
+  }
+}
+
+export class Integer {
+  private _type: string = "Integer";
+  private _value: string;
+
+  get value(): number {
+    return stringToNumber(this._value);
+  }
+
+  set value(value: number) {
+    this._value = numberToString(value);
+  }
+
+  constructor(value: number) {
+    this._value = numberToString(value);
+  }
+}
+
+export class UUID {
+  private _type: string = "UUID";
+  private _value: string;
+
+  get value(): BigInt {
+    return stringToBigInt(this._value);
+  }
+
+  set value(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+
+  constructor(value: BigInt) {
+    this._value = bigIntToString(value);
+  }
+}
+
+export class Blob {
+  private _type: Kind = Kind.Blob;
+  private _hash: string;
+
+  get value(): Uint8Array {
+    return stringToUint8Array(this._hash);
+  }
+
+  set value(hash: Uint8Array) {
+    this._hash = uint8ArrayToString(hash);
+  }
+
+  public get type(): Kind {
+    return this._type;
+  }
+
+  constructor(hash: Uint8Array) {
+    this._hash = uint8ArrayToString(hash);
+  }
+}
+
+export class Bytes {
+  private _type: string = "Bytes";
+  private _value: string;
+
+  get value(): Uint8Array {
+    return stringToUint8Array(this._value);
+  }
+
+  set value(value: Uint8Array) {
+    this._value = uint8ArrayToString(value);
+  }
+
+  public get type(): string {
+    return this._type;
+  }
+
+  constructor(value: Uint8Array) {
+    this._value = uint8ArrayToString(value);
+  }
+}
---
+++ tests/model/manifest_sbor_value_serialization.test.ts
@@ -0,0 +1,245 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import { ManifestSborValue } from "../../src";
+import { describe, expect, test } from "vitest";
+import { deserialize, serialize } from "../../src/utils";
+
+describe.each([
+  {
+    expectedObject: new ManifestSborValue.Bool(true),
+    expectedSerialization: `{"type":"Bool","value":true}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Bool(false),
+    expectedSerialization: `{"type":"Bool","value":false}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.U8(1),
+    expectedSerialization: `{"type":"U8","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.U16(1),
+    expectedSerialization: `{"type":"U16","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.U32(1),
+    expectedSerialization: `{"type":"U32","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.U64(1),
+    expectedSerialization: `{"type":"U64","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.U128(BigInt("1")),
+    expectedSerialization: `{"type":"U128","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.I8(1),
+    expectedSerialization: `{"type":"I8","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.I16(1),
+    expectedSerialization: `{"type":"I16","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.I32(1),
+    expectedSerialization: `{"type":"I32","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.I64(1),
+    expectedSerialization: `{"type":"I64","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.I128(BigInt("1")),
+    expectedSerialization: `{"type":"I128","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.String("Scrypto"),
+    expectedSerialization: `{"type":"String","value":"Scrypto"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Enum(1),
+    expectedSerialization: `{"type":"Enum","variant":"1","fields":[]}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Enum(1, [
+      new ManifestSborValue.U8(1),
+    ]),
+    expectedSerialization: `{"type":"Enum","variant":"1","fields":[{"type":"U8","value":"1"}]}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Array(ManifestSborValue.Kind.U8, [
+      new ManifestSborValue.U8(1),
+      new ManifestSborValue.U8(2),
+      new ManifestSborValue.U8(3),
+    ]),
+    expectedSerialization: `{"type":"Array","element_kind":"U8","elements":[{"type":"U8","value":"1"},{"type":"U8","value":"2"},{"type":"U8","value":"3"}]}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Map(
+      ManifestSborValue.Kind.U8,
+      ManifestSborValue.Kind.String,
+      [
+        [new ManifestSborValue.U8(65), new ManifestSborValue.String("A")],
+        [new ManifestSborValue.U8(66), new ManifestSborValue.String("B")],
+      ]
+    ),
+    expectedSerialization: `{"type":"Map","key_value_kind":"U8","value_value_kind":"String","entries":[[{"type":"U8","value":"65"},{"type":"String","value":"A"}],[{"type":"U8","value":"66"},{"type":"String","value":"B"}]]}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Map(
+      ManifestSborValue.Kind.U8,
+      ManifestSborValue.Kind.String,
+      [
+        [new ManifestSborValue.U8(65), new ManifestSborValue.String("A")],
+        [new ManifestSborValue.U8(66), new ManifestSborValue.String("B")],
+      ]
+    ),
+    expectedSerialization: `{"type":"Map","key_value_kind":"U8","value_value_kind":"String","entries":[[{"type":"U8","value":"65"},{"type":"String","value":"A"}],[{"type":"U8","value":"66"},{"type":"String","value":"B"}]]}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Tuple([
+      new ManifestSborValue.Tuple([
+        new ManifestSborValue.U8(1),
+        new ManifestSborValue.String("Something"),
+      ]),
+    ]),
+    expectedSerialization: `{"type":"Tuple","elements":[{"type":"Tuple","elements":[{"type":"U8","value":"1"},{"type":"String","value":"Something"}]}]}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Decimal(BigInt("1")),
+    expectedSerialization: `{"type":"Decimal","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.PreciseDecimal(BigInt("1")),
+    expectedSerialization: `{"type":"PreciseDecimal","value":"1"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Address(
+      "component_rdx1qtkryz5scup945usk39qjc2yjh6l5zsyuh8t7v5pk0tsrdcazt"
+    ),
+    expectedSerialization: `{"type":"Address","address":"component_rdx1qtkryz5scup945usk39qjc2yjh6l5zsyuh8t7v5pk0tsrdcazt"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Address(
+      "resource_rdx1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqy99qqm"
+    ),
+    expectedSerialization: `{"type":"Address","address":"resource_rdx1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqy99qqm"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Address(
+      "package_rdx1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqpqqzrhqe8"
+    ),
+    expectedSerialization: `{"type":"Address","address":"package_rdx1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqpqqzrhqe8"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Bucket(
+      new ManifestSborValue.String("bucket")
+    ),
+    expectedSerialization: `{"type":"Bucket","identifier":{"type":"String","value":"bucket"}}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Bucket(new ManifestSborValue.U32(1)),
+    expectedSerialization: `{"type":"Bucket","identifier":{"type":"U32","value":"1"}}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Proof(
+      new ManifestSborValue.String("proof")
+    ),
+    expectedSerialization: `{"type":"Proof","identifier":{"type":"String","value":"proof"}}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Proof(new ManifestSborValue.U32(1)),
+    expectedSerialization: `{"type":"Proof","identifier":{"type":"U32","value":"1"}}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.NonFungibleLocalId(
+      new ManifestSborValue.UUID(
+        BigInt("241008287272164729465721528295504357972")
+      )
+    ),
+    expectedSerialization: `{"type":"NonFungibleLocalId","value":{"type":"UUID","value":"241008287272164729465721528295504357972"}}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.NonFungibleLocalId(
+      new ManifestSborValue.Integer(1)
+    ),
+    expectedSerialization: `{"type":"NonFungibleLocalId","value":{"type":"Integer","value":"1"}}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.NonFungibleLocalId(
+      new ManifestSborValue.String("Scrypto")
+    ),
+    expectedSerialization: `{"type":"NonFungibleLocalId","value":{"type":"String","value":"Scrypto"}}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.NonFungibleLocalId(
+      new ManifestSborValue.Bytes(new Uint8Array([0x01, 0x02, 0x03, 0x04]))
+    ),
+    expectedSerialization: `{"type":"NonFungibleLocalId","value":{"type":"Bytes","value":"01020304"}}`,
+  },
+  {
+    expectedObject: ManifestSborValue.Expression.entireWorktop(),
+    expectedSerialization: `{"type":"Expression","value":"ENTIRE_WORKTOP"}`,
+  },
+  {
+    expectedObject: ManifestSborValue.Expression.entireAuthZone(),
+    expectedSerialization: `{"type":"Expression","value":"ENTIRE_AUTH_ZONE"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Blob(
+      new Uint8Array([
+        210, 141, 44, 55, 16, 96, 31, 188, 9, 112, 0, 236, 115, 69, 86, 147,
+        244, 134, 29, 192, 235, 124, 144, 216, 130, 31, 42, 19, 246, 23, 49, 62,
+      ])
+    ),
+    expectedSerialization: `{"type":"Blob","hash":"d28d2c3710601fbc097000ec73455693f4861dc0eb7c90d8821f2a13f617313e"}`,
+  },
+  {
+    expectedObject: new ManifestSborValue.Bytes(
+      new Uint8Array([
+        210, 141, 44, 55, 16, 96, 31, 188, 9, 112, 0, 236, 115, 69, 86, 147,
+        244, 134, 29, 192, 235, 124, 144, 216, 130, 31, 42, 19, 246, 23, 49, 62,
+      ])
+    ),
+    expectedSerialization: `{"type":"Bytes","value":"d28d2c3710601fbc097000ec73455693f4861dc0eb7c90d8821f2a13f617313e"}`,
+  },
+])(
+  "Serialization test for $expectedSerialization",
+  ({ expectedObject, expectedSerialization }) => {
+    test(`${expectedObject} is serialized as expected`, () => {
+      // Act
+      let actual_serialization = serialize(expectedObject);
+
+      // Assert
+      expect(actual_serialization).toEqual(expectedSerialization);
+    });
+
+    test(`${expectedSerialization} is deserialized as expected`, () => {
+      // Act
+      let actualObject = deserialize(
+        expectedSerialization,
+        // @ts-ignore
+        expectedObject.constructor
+      );
+
+      // Assert
+      expect(actualObject).toEqual(expectedObject);
+    });
+  }
+);
