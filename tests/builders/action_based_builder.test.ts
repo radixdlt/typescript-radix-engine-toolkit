f8940152d1806a6be23ff7a501a42592242d3540
Omar <OmarAbdulla7@hotmail.com>
2023-04-11 19:31:03 +0300

    tests for action based builder

--- tests/builders/action_based_builder.test.ts
+++ tests/builders/action_based_builder.test.ts
@@ -82,4 +82,145 @@ describe("Action Builder Tests", () => {
     // Assert
     expect(transaction.signedIntent.intent.manifest).toEqual(expectedManifest);
   });
+
+  it("Simple action builder manifest aggregates withdraws as expected", async () => {
+    // Arrange
+    let privateKey = new PrivateKey.EddsaEd25519(
+      "d52618de62aa37a9fdac229614ca931d9e509e00cd01ff9f465e5dba5e17be8b"
+    );
+
+    let account1 =
+      "account_sim1qjdkmaevmu7ggs3jyruuykx2u5c2z7mp6wjk5f5tpy6swx5788";
+    let account2 =
+      "account_sim1qj0vpwp3l3y8jhk6nqtdplx4wh6mpu8mhu6mep4pua3q8tn9us";
+    let account3 =
+      "account_sim1qjj40p52dnww68e594c3jq6h3s8xr75fgcnpvlwmypjqmqamld";
+
+    let resourceAddress1 =
+      "resource_sim1qyw4pk2ecwecslf55dznrv49xxndzffnmpcwjavn5y7qyr2l73";
+    let resourceAddress2 =
+      "resource_sim1qymzzch4zj3k3emvtx0hxw98e4zktx96z2ewtsrqjprslqfpu7";
+
+    // Act
+    let transaction: NotarizedTransaction = await ActionTransactionBuilder.new(
+      10,
+      20,
+      242,
+      account1
+    ).then((builder) => {
+      return builder
+        .fungibleResourceTransfer(account1, account2, resourceAddress1, 100)
+        .fungibleResourceTransfer(account1, account3, resourceAddress1, 200)
+        .build(privateKey.publicKey(), privateKey);
+    });
+
+    let expectedManifest = new ManifestBuilder()
+      .callMethod(
+        new ManifestAstValue.Address(account1),
+        new ManifestAstValue.String("lock_fee_and_withdraw"),
+        [
+          new ManifestAstValue.Decimal(5),
+          new ManifestAstValue.Address(resourceAddress1),
+          new ManifestAstValue.Decimal(300),
+        ]
+      )
+      .takeFromWorktopByAmount(
+        new ManifestAstValue.Address(resourceAddress1),
+        new ManifestAstValue.Decimal(100),
+        (builder, bucket) => {
+          return builder.callMethod(
+            new ManifestAstValue.Address(account2),
+            new ManifestAstValue.String("deposit"),
+            [bucket]
+          );
+        }
+      )
+      .takeFromWorktopByAmount(
+        new ManifestAstValue.Address(resourceAddress1),
+        new ManifestAstValue.Decimal(200),
+        (builder, bucket) => {
+          return builder.callMethod(
+            new ManifestAstValue.Address(account3),
+            new ManifestAstValue.String("deposit"),
+            [bucket]
+          );
+        }
+      )
+      .build();
+
+    console.log("actual", transaction.signedIntent.intent.manifest.toString());
+    console.log("expected", expectedManifest.toString());
+
+    // Assert
+    expect(transaction.signedIntent.intent.manifest).toEqual(expectedManifest);
+  });
+
+  it("Simple action builder manifest aggregates deposits as expected", async () => {
+    // Arrange
+    let privateKey = new PrivateKey.EddsaEd25519(
+      "d52618de62aa37a9fdac229614ca931d9e509e00cd01ff9f465e5dba5e17be8b"
+    );
+
+    let account1 =
+      "account_sim1qjdkmaevmu7ggs3jyruuykx2u5c2z7mp6wjk5f5tpy6swx5788";
+    let account2 =
+      "account_sim1qj0vpwp3l3y8jhk6nqtdplx4wh6mpu8mhu6mep4pua3q8tn9us";
+    let account3 =
+      "account_sim1qjj40p52dnww68e594c3jq6h3s8xr75fgcnpvlwmypjqmqamld";
+
+    let resourceAddress1 =
+      "resource_sim1qyw4pk2ecwecslf55dznrv49xxndzffnmpcwjavn5y7qyr2l73";
+    let resourceAddress2 =
+      "resource_sim1qymzzch4zj3k3emvtx0hxw98e4zktx96z2ewtsrqjprslqfpu7";
+
+    // Act
+    let transaction: NotarizedTransaction = await ActionTransactionBuilder.new(
+      10,
+      20,
+      242,
+      account1
+    ).then((builder) => {
+      return builder
+        .fungibleResourceTransfer(account1, account2, resourceAddress1, 100)
+        .fungibleResourceTransfer(account3, account2, resourceAddress1, 100)
+        .build(privateKey.publicKey(), privateKey);
+    });
+
+    let expectedManifest = new ManifestBuilder()
+      .callMethod(
+        new ManifestAstValue.Address(account1),
+        new ManifestAstValue.String("lock_fee_and_withdraw"),
+        [
+          new ManifestAstValue.Decimal(5),
+          new ManifestAstValue.Address(resourceAddress1),
+          new ManifestAstValue.Decimal(100),
+        ]
+      )
+      .callMethod(
+        new ManifestAstValue.Address(account3),
+        new ManifestAstValue.String("withdraw"),
+        [
+          new ManifestAstValue.Address(resourceAddress1),
+          new ManifestAstValue.Decimal(100),
+        ]
+      )
+      .takeFromWorktopByAmount(
+        new ManifestAstValue.Address(resourceAddress1),
+        new ManifestAstValue.Decimal(200),
+        (builder, bucket) => {
+          return builder.callMethod(
+            new ManifestAstValue.Address(account2),
+            new ManifestAstValue.String("deposit"),
+            [bucket]
+          );
+        }
+      )
+      .build();
+
+    console.log("actual", transaction.signedIntent.intent.manifest.toString());
+    console.log("expected", expectedManifest.toString());
+
+    // Assert
+    expect(transaction.signedIntent.intent.manifest).toEqual(expectedManifest);
+  });
 });
