a3040a7bf03392e0ab24504c61edc561340a430b
Omar <OmarAbdulla7@hotmail.com>
2023-08-03 02:25:35 +0300

    Update toolkit to latest Scrypto

--- tests/raw.test.ts
+++ tests/raw.test.ts
@@ -27,7 +27,7 @@ describe("Raw Radix Engine Toolkit Tests", () => {
     const buildInformation = ret.buildInformation({});
 
     // Assert
-    expect(buildInformation.version).toEqual("0.12.0");
+    expect(buildInformation.version).toEqual("0.12.0-fig.1");
   });
 
   it("Raw Radix Engine Toolkit throws exceptions for invocation errors", async () => {
